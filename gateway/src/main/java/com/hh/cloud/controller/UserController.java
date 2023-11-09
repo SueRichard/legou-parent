10d15d7394dcbbd8a3ad60099e62e81632e2f651
huanghao <hanleideying@126.com>
2023-11-09 08:40:55 +0800

    网关增加jwt登录、jwt鉴权过滤器

---
+++ gateway/src/main/java/com/hh/cloud/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.hh.cloud.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hh.cloud.client.UserClient;
+import com.hh.cloud.util.BCrypt;
+import com.hh.cloud.util.JwtUtil;
+import com.hh.legou.security.po.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author hh
+ * @version 1.0
+ * @time 09/11/2023 08:00
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserClient userClient;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @RequestMapping("/login")
+    public ResponseEntity login(String username, String password) throws JsonProcessingException {
+        User user = userClient.getByUserName(username);
+        //用户名不存在
+        if (user == null) {
+            return new ResponseEntity("用户名错误", HttpStatus.UNAUTHORIZED);
+        }
+        //校验密码是否一致
+        if (BCrypt.checkpw(password, user.getPassword())) {
+            //认证成功，准备令牌信息
+            Map<String, Object> info = new HashMap<>();
+            info.put("role", "user");
+            info.put("success", "true");
+            info.put("username", username);
+            //颁发令牌，过期时间默一小时
+            String jwt = JwtUtil.createJWT(UUID.randomUUID().toString(), om.writeValueAsString(info), null);
+            return ResponseEntity.ok(jwt);
+        } else {
+            //密码错误
+            return new ResponseEntity("密码错误", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+}
---
+++ gateway/src/main/java/com/hh/cloud/filter/AuthorizeFilter.java
@@ -0,0 +1,66 @@
+package com.hh.cloud.filter;
+
+import com.hh.cloud.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author hh
+ * @version 1.0
+ * @time 09/11/2023 08:20
+ */
+@Component
+public class AuthorizeFilter implements GlobalFilter, Ordered {
+    private static final String AUTHORIZE_TOKEN = "Authorization";
+
+    /**
+     * @param exchange
+     * @param chain
+     * @return
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        //登录放行
+        if (request.getURI().getPath().startsWith("/user/login")) {
+            return chain.filter(exchange);
+        }
+        //头部获得令牌
+        String token = request.getHeaders().getFirst(AUTHORIZE_TOKEN);
+        if (StringUtils.isEmpty(token)) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+        //校验令牌
+        try {
+            Claims claims = JwtUtil.parseJWT(token);
+            //解析用户角色，针对不同角色访问对应的微服务
+        } catch (Exception e) {
+            e.printStackTrace();
+            //解析失败
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+        //能够解析，放行
+        return chain.filter(exchange);
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public int getOrder() {
+        //值越小，在过滤器链中优先级越高
+        return 0;
+    }
+}
