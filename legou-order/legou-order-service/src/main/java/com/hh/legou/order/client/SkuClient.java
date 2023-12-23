d27b54ce6f044f44399bd5ce6f20a83c808d1cba
huanghao <hanleideying@126.com>
2023-12-23 15:44:48 +0800

    fallbackFactory处理异常，普通fallback无法处理

--- legou-order/legou-order-service/src/main/java/com/hh/legou/order/client/SkuClient.java
+++ legou-order/legou-order-service/src/main/java/com/hh/legou/order/client/SkuClient.java
@@ -3,6 +3,7 @@ package com.hh.legou.order.client;
 import com.hh.legou.item.api.SkuApi;
 import com.hh.legou.item.po.Sku;
 import com.hh.legou.item.po.Spu;
+import feign.hystrix.FallbackFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -16,7 +17,10 @@ import java.util.List;
  * @version 1.0
  * @time 19/11/2023 17:01
  */
-@FeignClient(name = "item-service", fallback = SkuClient.SkuClientFallback.class)
+
+//fallback和fallbackFactory只能存在一个
+@FeignClient(name = "item-service", fallbackFactory = SkuClient.SkuClientFallbackFactory.class)
+//@FeignClient(name = "item-service", fallback = SkuClient.SkuClientFallback.class)
 public interface SkuClient extends SkuApi {
     @Component
     @RequestMapping("/sku-fallback")
@@ -34,6 +38,32 @@ public interface SkuClient extends SkuApi {
         public Sku edit(Long id) {
             log.error("异常发生，进入fallback方法");
             return null;
+        }
+    }
+
+    /**
+     * fallbackFactory 可以处理异常
+     */
+    @Component
+    @RequestMapping("/sku-fallback-factory")
+    class SkuClientFallbackFactory implements FallbackFactory<SkuClient> {
+        Logger log = LoggerFactory.getLogger(SkuClientFallbackFactory.class);
+
+        @Override
+        public SkuClient create(Throwable cause) {
+            cause.printStackTrace();
+            log.error(cause.getMessage());
+            return new SkuClient() {
+                @Override
+                public List<Sku> selectSkusBySpuId(Long SpuId) {
+                    return null;
+                }
+
+                @Override
+                public Sku edit(Long id) {
+                    return null;
+                }
+            };
         }
     }
 }
