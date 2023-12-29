cf16ce43b0794a2cbadaf83cff6024d70922a7d9
huanghao <hanleideying@126.com>
2023-12-29 09:41:55 +0800

    订单添加接口 创建订单

--- legou-order/legou-order-service/src/main/java/com/hh/legou/order/controller/OrderController.java
+++ legou-order/legou-order-service/src/main/java/com/hh/legou/order/controller/OrderController.java
@@ -2,10 +2,17 @@ package com.hh.legou.order.controller;
 
 import com.hh.legou.core.controller.BaseController;
 import com.hh.legou.core.service.ICrudService;
+import com.hh.legou.order.config.TokenDecode;
 import com.hh.legou.order.po.Order;
 import com.hh.legou.order.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * @author hh
@@ -15,4 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/order")
 public class OrderController extends BaseController<IOrderService, Order> {
+
+    @Autowired
+    private TokenDecode tokenDecode;
+
+    @PostMapping("/add")
+    public ResponseEntity add(@RequestBody Order order) throws IOException {
+        String username = tokenDecode.getUserInfo().get("user_name");
+        order.setUsername(username);
+        service.add(order);
+        return ResponseEntity.ok("添加成功");
+    }
 }
--- legou-order/legou-order-service/src/main/java/com/hh/legou/order/service/impl/OrderServiceImpl.java
+++ legou-order/legou-order-service/src/main/java/com/hh/legou/order/service/impl/OrderServiceImpl.java
@@ -1,9 +1,17 @@
 package com.hh.legou.order.service.impl;
 
+import com.hh.legou.common.utils.IdWorker;
 import com.hh.legou.core.service.impl.CrudServiceImpl;
+import com.hh.legou.order.dao.IOrderItemDao;
 import com.hh.legou.order.po.Order;
+import com.hh.legou.order.po.OrderItem;
 import com.hh.legou.order.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author hh
@@ -13,8 +21,50 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderServiceImpl extends CrudServiceImpl<Order> implements IOrderService {
 
+    @Autowired
+    private IdWorker idWorker;
+
+    @Autowired
+    private IOrderItemDao orderItemDao;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
     public void add(Order order) {
+        //1.添加订单主表数据
+        order.setId(idWorker.nextId());
+        //2.循环购物车，添加订单明细数据
+        List<OrderItem> cartList = redisTemplate.boundHashOps("Cart_" + order.getUsername()).values();
+        Long totalNum = 0l;//订单总数量
+        Long totalMoney = 0l;//订单总金额
+        for (OrderItem orderItem : cartList) {
+            //计算总数相关
+            totalNum += orderItem.getNum();
+            totalMoney += orderItem.getPayMoney();
+
+            orderItem.setId(idWorker.nextId());//订单选项id
+            orderItem.setOrderId(order.getId());//订单id
+            orderItem.setIsReturn("0");//未退货
+            orderItemDao.insert(orderItem);
+        }
+        order.setTotalNum(totalNum);//设置总数量
+        order.setTotalMoney(totalMoney);//设置总金额
+        order.setPayMoney(totalMoney);//设置实付金额
+        order.setCreateTime(new Date());
+        order.setUpdateTime(order.getCreateTime());
+        order.setOrderStatus("0");//未完成
+        order.setPayStatus("0");//未支付
+        order.setConsignStatus("0");//未发货
+        order.setIsDelete("0");
+
+        getBaseMapper().insert(order);
+
+        //3.调用商品微服务减库存
+
+        //4.增加用户积分
+
+        //5.删除redis购物车数据
 
     }
 }
