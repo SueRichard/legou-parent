b6f3b37fdf1a54793fbfaafc9f3940bfe19c0ed7
huanghao <hanleideying@126.com>
2024-01-03 11:59:57 +0800

    秒杀微服务 interface创建，实体类部分

---
+++ legou-seckill/legou-seckill-interface/src/main/java/com/hh/legou/seckill/po/SecKillGoods.java
@@ -0,0 +1,16 @@
+package com.hh.legou.seckill.po;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.hh.legou.core.po.BaseEntity;
+import lombok.Data;
+
+/**
+ * @author hh
+ * @version 1.0
+ * @time 03/01/2024 11:58
+ */
+@Data
+@TableName("seckill_goods_")
+public class SecKillGoods extends BaseEntity {
+
+}
