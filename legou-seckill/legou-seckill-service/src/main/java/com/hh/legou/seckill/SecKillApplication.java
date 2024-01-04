f53ecacffd099b09aa42ab8ad02297bde91d18b6
huanghao <hanleideying@126.com>
2024-01-04 16:34:27 +0800

    秒杀微服务 工程搭建完毕

---
+++ legou-seckill/legou-seckill-service/src/main/java/com/hh/legou/seckill/SecKillApplication.java
@@ -0,0 +1,22 @@
+package com.hh.legou.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author hh
+ * @version 1.0
+ * @time 04/01/2024 16:30
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+public class SecKillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecKillApplication.class, args);
+    }
+}
